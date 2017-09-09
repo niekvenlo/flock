fe61c3e0c76540c54c94fc428522bc9eb602cd05
Nick Beaujean <niekvenlo@gmail.com>
2017-09-08 18:52:57 -0700

    Add canvas graphics, central attractor. Looks ok

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function multiplyVector (vector, multiplier) {
 }
 function normalizeVector (vector) {
   let sumLength = vectorLength(vector)
+  console.assert(vector.x !== 0 || vector.y !== 0 || vector.z !== 0)
   return {
     x: vector.x / sumLength,
     y: vector.y / sumLength,
@@ -46,7 +47,7 @@ function negateVector (vector) {
   }
 }
 function randomNumber (n) {
-  return Math.floor(Math.random() * n)
+  return Math.random() * n
 }
 function withinReach (boid, currentBoid, maxDistance) {
   // Currently doesn't filter out the currentBoid
@@ -55,13 +56,14 @@ function withinReach (boid, currentBoid, maxDistance) {
 
 class Boid {
   constructor (a, b, c, d, e, f) {
-    a = a || randomNumber(200)
-    b = b || randomNumber(200)
-    c = c || randomNumber(200)
+    let spread = 2000
+    a = a || spread / 2 - randomNumber(spread)
+    b = b || spread / 2 - randomNumber(spread)
+    c = c || spread / 2 - randomNumber(spread)
 
-    d = d || randomNumber(10)
-    e = e || randomNumber(4)
-    f = f || randomNumber(1)
+    d = d || 1
+    e = e || 0.5 - randomNumber(1)
+    f = f || 0
 
     this.position = {x: a, y: b, z: c}
     this.velocity = {x: d, y: e, z: f}
@@ -69,21 +71,21 @@ class Boid {
   }
 }
 
-let boids = new Array(400).fill(1).map(element => new Boid())
-
 let parameters = {
   range: {
-    alignment: 100,
-    cohesion: 100,
-    repulsion: 100
+    alignment: 20,
+    cohesion: 200,
+    repulsion: 40
   },
   weight: {
-    alignment: 1,
-    cohesion: 1,
-    repulsion: 1
+    alignment: 0.1,
+    cohesion: 0.001,
+    repulsion: 0.001,
+    centralize: 0.00001
   },
-  timeDelta: 10,
-  boidSpeed: 0.1
+  timeDelta: 25,
+  boidSpeed: 0.2,
+  numberOfBoids: 500
 }
 
 function getAlignment (currentBoid) {
@@ -133,10 +135,14 @@ function getRepulsion (currentBoid) {
         let distanceVector = differenceVector(everyOtherBoidPosition, currentBoid.position)
         return sumVectors(resultant, distanceVector)
       })
+      // console.log(vectorLength(sumOfBoids))
     return negateVector(normalizeVector(sumOfBoids))
   } catch (e) {
     return { x: 0, y: 0, z: 0 }
   }
+}
+function getCentralize (currentBoid) {
+  return differenceVector({x: 0, y: 0, z: 0}, currentBoid.position)
 }
 
 function updateBoid (boid) {
@@ -144,11 +150,13 @@ function updateBoid (boid) {
   let align = multiplyVector(getAlignment(boid), parameters.weight.alignment)
   let cohere = multiplyVector(getCohesion(boid), parameters.weight.cohesion)
   let repel = multiplyVector(getRepulsion(boid), parameters.weight.repulsion)
+  let centre = multiplyVector(getCentralize(boid), parameters.weight.centralize)
 
   let updatedBoid = {}
-  // velocity = normalized sum * speed
+  // velocity = normalized sum * speed + a vector toward zero
   let normalVelocity = normalizeVector(sumVectors(boid.velocity, align, cohere, repel))
-  updatedBoid.velocity = multiplyVector(normalVelocity, parameters.boidSpeed)
+  let centreAdjustedVelocity = sumVectors(normalVelocity, centre)
+  updatedBoid.velocity = multiplyVector(centreAdjustedVelocity, parameters.boidSpeed)
   // position = position + (velocity * timeDelta)
   let movement = multiplyVector(updatedBoid.velocity, parameters.timeDelta)
   updatedBoid.position = sumVectors(boid.position, movement)
@@ -157,14 +165,36 @@ function updateBoid (boid) {
   return updatedBoid
 }
 
-// Quick performance sanity check
-let t = Date.now()
-let steps = 100
-console.log(boids[0].position)
-for (let i = 0; i < steps; i++) {
-  boids = boids.map(boid => updateBoid(boid))
-  if (i % (steps / 10) === 0) {
-    console.log(boids[0].position)
-  }
+function createCanvas () {
+  let canvas = document.createElement('canvas')
+  let ctx = canvas.getContext('2d')
+  canvas.height = window.innerHeight
+  canvas.width = window.innerWidth
+  document.body.appendChild(canvas)
+  return {canvas, ctx}
 }
-console.log((Date.now() - t) / steps)
+function draw (boid, canvas, ctx) {
+  let {x, y} = boid.position
+  let sx = x + (canvas.width / 2)
+  let sy = y + (canvas.height / 2)
+  let boidSize = 2
+  // boidSize = Math.abs(10 + Math.round(z / 100))
+  ctx.beginPath()
+  ctx.arc(sx, sy, boidSize, 0, 2 * Math.PI)
+  ctx.fill()
+  // ctx.fillRect(sx, sy, boidSize, boidSize)
+}
+
+function tick (canvas, ctx) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  boids.forEach(boid => draw(boid, canvas, ctx))
+  boids = boids.map(boid => updateBoid(boid))
+}
+
+function start () {
+  let {canvas, ctx} = createCanvas()
+  setInterval(tick.bind(this, canvas, ctx), parameters.timeDelta)
+}
+
+let boids = new Array(parameters.numberOfBoids).fill(1).map(element => new Boid())
+window.onload = start
